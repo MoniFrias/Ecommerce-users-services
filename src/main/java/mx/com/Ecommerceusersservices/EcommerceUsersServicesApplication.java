54f24627b59d636016a715bda5f819f1d1e9d86b
MoniFrias <friasmoni18@gmail.com>
2023-05-12 14:19:25 -0600

    Create users-services

---
+++ src/main/java/mx/com/Ecommerceusersservices/EcommerceUsersServicesApplication.java
@@ -0,0 +1,13 @@
+package mx.com.Ecommerceusersservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUsersServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceUsersServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/Ecommerceusersservices/EcommerceUsersServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.Ecommerceusersservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUsersServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
